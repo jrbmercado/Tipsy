30338804a6e3854958cdcab915f45f5b22d107bb
Justin Mercado <justinreianmercado@gmail.com>
2022-08-28 10:04:18 -0700

    Added IBOutlets and IBAction for elements in CalculatorViewController

---
+++ Tipsy/CalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Justin Mercado on 08/28/2022
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var zeroPercentButton: UIButton!
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var twentyPercentButton: UIButton!
+    
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
