2571be95e556d927ebac35fd94fa7800904a27fd
Justin Mercado <justinreianmercado@gmail.com>
2022-08-28 10:35:12 -0700

    Added ResultsViewController

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Justin Mercado on 8/28/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+    
+    
+}
