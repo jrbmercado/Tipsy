60f4745462141ba0a4735d69ba75bf6f4e76491b
Justin Mercado <justinreianmercado@gmail.com>
2022-08-28 10:49:04 -0700

    Added logic to button selection and figuring out how much tip to apply

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -17,12 +17,38 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        // Check to see what the button that triggered this IBAction is by checking the button's title text label
+        if(sender.titleLabel?.text == "0%"){ // If it is 0%, show selected then deselect all other buttons
+            zeroPercentButton.isSelected = true
+            tenPercentButton.isSelected = false
+            twentyPercentButton.isSelected = false
+        }
+        else if (sender.titleLabel?.text == "10%"){ // If it is 10%, show selected then deselect all other buttons
+            zeroPercentButton.isSelected = false
+            tenPercentButton.isSelected = true
+            twentyPercentButton.isSelected = false
+        }
+        else{ // If it is 20%, show selected then deselect all other buttons
+            zeroPercentButton.isSelected = false
+            tenPercentButton.isSelected = false
+            twentyPercentButton.isSelected = true
+        }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        // Check to see which button is currently selected, there will only be one button selected at a time
+        if(zeroPercentButton.isSelected){
+            print("0.0")
+        }
+        else if(tenPercentButton.isSelected){
+            print("0.1")
+        }
+        else{
+            print("0.2")
+        }
     }
     
 }
